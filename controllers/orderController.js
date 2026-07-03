60816322733f3a3d419a75a36aa4c2fc5ec58e91
SatwikTatikonda <satwk.tatikonda@gmail.com>
2026-07-03 20:26:58 +0530

    pharmacy APIs

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -19,6 +19,30 @@ const getPendingOrders = async (req, res) => {
   }
 };
 
+
+
+// Get Processing Orders
+const getProcessingOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({
+      orderStatus: "Processing"
+    });
+
+    res.status(200).json({
+      success: true,
+      totalOrders: orders.length,
+      data: orders
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
 module.exports = {
   getPendingOrders,
+  getProcessingOrders
 };
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 const {
   getPendingOrders,
+  getProcessingOrders
 } = require("../controllers/orderController");
 
 // GET Pending Orders
 router.get("/pending", getPendingOrders);
+router.get("/processing", getProcessingOrders);
 
 module.exports = router;
