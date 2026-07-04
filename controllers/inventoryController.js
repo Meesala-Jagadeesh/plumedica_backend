fa9036cf4ae58f7386d516d88719b71d45b3c32f
SatwikTatikonda <satwk.tatikonda@gmail.com>
2026-07-04 11:11:21 +0530

    pharmacyAPIs

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -39,7 +39,29 @@ const getAnalgesicMedicines = async (req, res) => {
   }
 };
 
+
+// Get Respiratory Medicines
+const getRespiratoryMedicines = async (req, res) => {
+  try {
+    const medicines = await Inventory.find({
+      category: "Respiratory",
+    });
+
+    res.status(200).json({
+      success: true,
+      totalMedicines: medicines.length,
+      data: medicines,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
     getAllInventory,
     getAnalgesicMedicines,
+    getRespiratoryMedicines,
 };
--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -5,11 +5,14 @@ const router = express.Router();
 const {
     getAllInventory,
     getAnalgesicMedicines,
+    getRespiratoryMedicines,
 } = require("../controllers/inventoryController");
 
 // Get All Medicines
 router.get("/all", getAllInventory);
 // Get Analgesic Medicines
 router.get("/category/Analgesic", getAnalgesicMedicines);
+// Get Respiratory Medicines
+router.get("/category/Respiratory", getRespiratoryMedicines);
 
 module.exports = router;
